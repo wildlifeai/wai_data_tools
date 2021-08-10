9cc41092f89b0c585bd471d401770d8f0df19d33
David Andersson <david.aderup@gmail.com>
2021-08-10 09:44:15 +0200

    finished excel reading and filtering to label folders functionality

--- read_excel.py
+++ read_excel.py
@@ -1,9 +1,9 @@
 
 from pathlib import Path
 from typing import Dict, Union
+import shutil
 
 import pandas as pd
-import matplotlib.pyplot as plt
 
 
 def read_excel_to_dataframe(excel_file_path: Path) -> Union[pd.DataFrame, Dict[str, pd.DataFrame]]:
@@ -30,13 +30,41 @@ def append_rows_from_dataframe_dictionary(dataframe_dict: Dict[str, pd.DataFrame
     return merged_dataframe
 
 
-excel_file_path = Path(r"C:\Users\david\Desktop\wildlife.ai\ww_labels.xlsx")
+def copy_files_to_label_folder(file_dataframe: pd.DataFrame, data_dir: Path, base_dir: Path):
+    """
+    Copies data files into folders based on the object label.
+    :param file_dataframe: Dataframe with information about data files
+    :param data_dir: Directory where data is stored, should contain the WW01, WW02, ... folders.
+    :param base_dir: Directory to store the new label folders
+    """
 
-content = read_excel_to_dataframe(excel_file_path=excel_file_path)
-dataframe = append_rows_from_dataframe_dictionary(dataframe_dict=content)
+    labels = file_dataframe["label"].unique()
+    for label in labels:
+        label_dir = base_dir / label
+        label_dir.mkdir(exist_ok=True)
 
-# remove nothing label
-filtered_df = dataframe[dataframe["label"] != "nothing"]
+    for ind, df_row in file_dataframe.iterrows():
+        filename = df_row["filename"]
+        folder = df_row["folder"]
+        label = df_row["label"]
 
-filtered_df["label"].value_counts().plot(kind="barh")
-plt.show()
+        src = data_dir / folder / filename
+        dst = base_dir / label / filename
+        shutil.copy(str(src), str(dst))
+
+
+def filter_raw_data_to_label_folders(excel_file_path: Path,
+                                     raw_data_dir: Path,
+                                     new_data_dir: Path):
+    """
+    Filters the raw data .mjpg files from the WW0X folders to a new folder structure based on labels.
+    :param excel_file_path: Path to the excel file with label information
+    :param raw_data_dir: Path to the directory containing the WW0X folders.
+    :param new_data_dir: Directory to place the label folders
+    """
+
+    content = read_excel_to_dataframe(excel_file_path=excel_file_path)
+    dataframe = append_rows_from_dataframe_dictionary(dataframe_dict=content)
+
+    copy_files_to_label_folder(file_dataframe=dataframe, data_dir=raw_data_dir, base_dir=new_data_dir)
+
