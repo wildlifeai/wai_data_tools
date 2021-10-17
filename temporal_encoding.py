3f53ce69df43fba3b3cbe313eacd20762121643d
David Andersson <david.aderup@gmail.com>
2021-10-17 21:48:16 +0200

    refactored temporal encoding module by modularizing it and extending functionality to voxelwise mean removal

--- temporal_encoding.py
+++ temporal_encoding.py
@@ -7,69 +7,70 @@ from scipy.ndimage.filters import gaussian_filter
 import matplotlib.pyplot as plt
 
 
-def temporal_encoding(frame_dicts, window_size=-1):
+def temporal_encoding(frame_dicts, processing_config):
     new_frame_dicts = {}
 
-    # skip first two because we need two previous frames for encoding
-    for frame_ind in range(2, len(frame_dicts)):
+    remove_mean_from_frames(frame_dicts, window_size=40)
 
-        print(frame_ind)
-        curr_frame_dict = frame_dicts[frame_ind]
-        transformed_im = np.zeros_like(curr_frame_dict["img"]).astype(float)
+    raise AttributeError("hej")
 
-        # Construct mean and std image
-
-        if window_size == -1:
-            window_size = len(frame_dicts)
-
-        seq_arr = np.zeros((transformed_im.shape[0] , transformed_im.shape[1], window_size))
-
-        for window_ind in range(window_size):
-            seq_arr[:, :, window_ind] = frame_dicts[window_ind]["img"][:, :, 0]
-
-        mean_im = np.mean(seq_arr, axis=2)
-
-        std_im = np.std(seq_arr, axis=2)
-
-        transformed_im[:, :, 0] = frame_dicts[frame_ind]["img"][:, :, 0]
-
-        tmp = transformed_im[:, :, 0]
-
-        tmp = tmp - mean_im
-
-        tmp = tmp / std_im
-
-        tmp = tmp + abs(np.min(tmp))
-        tmp = tmp * 200 / np.max(tmp)
-        tmp = tmp.astype(int)
-
-        std_im += abs(std_im)
-        std_im *= 200 / np.max(std_im)
-        std_im = std_im.astype(int)
-
-        transformed_im[:, :, 1] = tmp
-        transformed_im[:, :, 2] = std_im
-
-        plt.imshow(transformed_im.astype(int))
-        plt.show()
-
-        raise AttributeError("hej")
-
-        new_frame_dict = {
-            "img": transformed_im,
-            "label": frame_dicts[frame_ind]["label"]
-        }
-        new_frame_dicts[frame_ind] = new_frame_dict
     return new_frame_dicts
 
 
-def create_grayscale_bg_removed(frame_dicts):
+def remove_mean_from_frames(frame_dicts, window_size):
+    window_inds_dict = get_window_inds(window_size=window_size,
+                                       n_frames=len(frame_dicts))
+
+    for (frame_ind, frame_dict), window_inds in zip(frame_dicts.items(), window_inds_dict.values()):
+        window_dict = {ind: frame_dicts[ind] for ind in window_inds}
+        mean_image = get_voxelwise_mean_for_frames(frame_dicts=window_dict)
+
+        proc_img = frame_dict["img"][:, :, 0].astype(float) - mean_image
+        proc_img = np.expand_dims(proc_img, axis=-1)
+        proc_img = scale_array_to_8bit(proc_img)
+        frame_dict["img"] = np.tile(proc_img, (1, 1, 3))
+        display_image(frame_dict["img"])
+
+
     pass
+
+
+def scale_array_to_8bit(img_array):
+
+    img_array += np.abs(np.min(img_array))
+
+    img_array *= (255/np.max(img_array))
+
+    return img_array.astype(int)
+
+def get_window_inds(window_size, n_frames):
+    frame_inds = np.arange(n_frames)
+    window_ind_dict = {}
+    for frame_ind in frame_inds:
+
+        if frame_ind + window_size <= n_frames:
+            start_ind = max(0, np.floor(frame_ind - window_size / 2))
+        else:
+            start_ind = n_frames - window_size
+        end_ind = min(n_frames, start_ind + window_size)
+        window_ind_dict[frame_ind] = np.arange(start_ind, end_ind)
+    return window_ind_dict
+
+
+def get_voxelwise_mean_for_frames(frame_dicts):
+    frame_imgs = [frame_dict["img"][:, :, 0].astype(float) for frame_dict in frame_dicts.values()]
+    frames_array = np.stack(frame_imgs, axis=0)
+    mean_image = np.mean(frames_array, axis=0)
+    return mean_image
+
+
+def display_image(image_array):
+    plt.imshow(image_array)
+    plt.show()
 
 
 def create_3_frame_rgb(frame_dicts):
     new_frame_dicts = {}
-
     # skip first two because we need two previous frames for encoding
     for curr_frame_ind in range(2, len(frame_dicts)):
         transformed_im = np.zeros_like(frame_dicts[curr_frame_ind]["img"]).astype(float)
@@ -90,11 +91,8 @@ for frame_dir in frame_dirs:
     print(frame_dir.name)
     frame_dicts = load_frames(frame_dir=frame_dir)
 
-    new_frame_dicts = temporal_encoding(frame_dicts=frame_dicts)
+    new_frame_dicts = temporal_encoding(frame_dicts=frame_dicts, processing_config=None)
 
     save_frames(frame_dir=frame_dir,
                 new_dir=Path(r"C:\Users\david\Desktop\wildlife.ai\curated-datasets\temporal-encoding-trials\bg-test"),
                 frame_dict=new_frame_dicts)
-
-
-s = ""
