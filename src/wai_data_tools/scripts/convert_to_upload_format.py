1eecf8b2565bf963909de9aa3333de457c92945f
David Andersson <david.andersson@hm.com>
2022-03-19 14:45:07 +0100

    added train test split and automatic infer filename to upload format

--- src/wai_data_tools/scripts/convert_to_upload_format.py
+++ src/wai_data_tools/scripts/convert_to_upload_format.py
@@ -6,32 +6,55 @@ import shutil
 import pandas as pd
 import tqdm
 
+from wai_data_tools import config
+from wai_data_tools.data import calc_test_split_indices
 
-def convert_file_structure_to_upload_format(src_root_dir: pathlib.Path, dst_root_dir: pathlib.Path) -> None:
+
+def convert_file_structure_to_upload_format(
+    src_root_dir: pathlib.Path, dst_root_dir: pathlib.Path, config_filepath: pathlib.Path
+) -> None:
     """Copy contents of a source file structure and stores it in a destination directoryin upload friendly format.
 
     Args:
         src_root_dir: Source root directory to read files from.
         dst_root_dir: Destination root directory to store new file structure.
+        config_filepath: Path to config file.
     """
+    logging.info("Reading frame information")
     df_frames = pd.read_csv(src_root_dir / "frame_information.csv")
     src_dataset_dir = src_root_dir / "dataset"
 
+    logging.info("Reading config")
+    config_dict = config.load_config(config_filepath=config_filepath)
+
+    test_split_size = config_dict["data_split"]["test_size"]
+
     logging.info("Creating new file structure for uploading to Edge Impulse")
+
+    video_names = df_frames["video_name"].unique()
+
+    test_file_inds = calc_test_split_indices(n_files=len(video_names), test_split_size=test_split_size)
+
+    test_video_names = video_names[test_file_inds]
 
     for _, frame_row in tqdm.tqdm(list(df_frames.iterrows())):
 
-        target_name = frame_row["contains_target"]
+        target_name = frame_row["target"]
+        video_name = frame_row["video_name"]
 
-        dst_target_dir = dst_root_dir / target_name
+        if video_name in test_video_names:
+            split_dir = "test"
+        else:
+            split_dir = "train"
+
+        dst_target_dir = dst_root_dir / split_dir
         dst_target_dir.mkdir(exist_ok=True, parents=True)
 
-        video_name = frame_row["filename"].split(".")[0]
+        src_frame_filename = frame_row["file_name"]
+        dst_frame_filename = f"{target_name}.{src_frame_filename}"
 
-        frame_filename = f"{video_name}___{frame_row['frame_ind']}.jpeg"
+        src_frame_filepath = src_dataset_dir / video_name / src_frame_filename
 
-        src_frame_filepath = src_dataset_dir / video_name / frame_filename
-
-        dst_frame_filepath = dst_target_dir / frame_filename
+        dst_frame_filepath = dst_target_dir / dst_frame_filename
 
         shutil.copy(str(src_frame_filepath), str(dst_frame_filepath))
