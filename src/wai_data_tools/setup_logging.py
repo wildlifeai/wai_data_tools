667b1a58edc48d5c9ba44bfb4ffcfec8182ef4bc
David Andersson <david.andersson@hm.com>
2022-04-17 13:52:36 +0200

    added optional parameters for logging to set logging directory or configure the logger

--- src/wai_data_tools/setup_logging.py
+++ src/wai_data_tools/setup_logging.py
@@ -1,22 +1,35 @@
 """This module is responsible for configuration of the logging."""
 import datetime
 import logging
+import os
 from pathlib import Path
+from typing import Optional
 
 
-def setup_logging():
-    """Configure the logging."""
-    logging_dir = Path(r"C:\git\wildlife_ai_file_filter\logs")
+def setup_logging(
+    logging_dir: Optional[Path] = None, logging_config_file: Optional[Path] = None
+) -> None:
+    """Initializes and configures the logger.
+
+    Args:
+        logging_dir: Optional path to store logs, if not given the logs will be stored in the working directory
+        logging_config_file: Optional path to config file for logger. If not given a lightweight default configuration will be used.
+    """
+    if logging_dir is None:
+        logging_dir = Path(os.getcwd())
 
     logging_dir.mkdir(exist_ok=True)
 
-    logging_filename = str(
-        logging_dir
-        / datetime.datetime.now().strftime("wildlife_log_%H_%M_%d_%m_%Y.log")
-    )
+    if logging_config_file:
+        logging.basicConfig(filename=logging_config_file)
+    else:
+        logging_filename = str(
+            logging_dir
+            / datetime.datetime.now().strftime("wildlife_log_%H_%M_%d_%m_%Y.log")
+        )
 
-    logging.basicConfig(
-        encoding="utf-8",
-        level=logging.INFO,
-        handlers=[logging.FileHandler(logging_filename), logging.StreamHandler()],
-    )
+        logging.basicConfig(
+            encoding="utf-8",
+            level=logging.INFO,
+            handlers=[logging.FileHandler(logging_filename), logging.StreamHandler()],
+        )
