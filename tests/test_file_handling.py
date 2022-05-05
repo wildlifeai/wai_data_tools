bfac5a9f7ec1e8dcc227a3e51dedaa3a2b95fe24
David Andersson <david.andersson@hm.com>
2022-05-05 21:24:40 +0200

    changed mock handling to monkeypatch from unittest.patch

--- tests/test_file_handling.py
+++ tests/test_file_handling.py
@@ -1,47 +1,43 @@
-"""Tests for io module."""
+"""Tests for file_handling module."""
 import pathlib
 from pathlib import Path
-from unittest.mock import MagicMock, patch
 
+import imageio
 import numpy as np
 import pandas as pd
-import pytest
 
 from wai_data_tools import file_handling
 
 
-@pytest.mark.parametrize(
-    argnames="frame_dir, df_frames, expected",
-    argvalues=[
-        (
-            Path("./test_path/A"),
-            pd.DataFrame(
-                {"video_name": ["A", "A", "C"], "frame_ind": [1, 2, 3], "target": ["class", "background", "background"]}
-            ),
-            {1: {"image": np.array([0]), "target": "class"}, 2: {"image": np.array([0]), "target": "background"}},
-        )
-    ],
-)
-def test_load_frames(frame_dir, df_frames, expected):
+def test_load_frames(monkeypatch):
     """Test case for load_frames function.
 
     Args:
-        frame_dir: Path to frame directory
-        df_frames: Dataframe with frame information
-        expected: Expected outcome
+        monkeypatch: Pytest monkeypatch fixture
     """
-    mocked_rglob = MagicMock(
-        return_value=[
+    frame_dir = Path("./test_path/A")
+
+    df_frames = pd.DataFrame(
+        {"video_name": ["A", "A", "C"], "frame_ind": [1, 2, 3], "target": ["class", "background", "background"]}
+    )
+
+    expected = {1: {"image": np.array([0]), "target": "class"}, 2: {"image": np.array([0]), "target": "background"}}
+
+    def mock_imread(*_, **__):
+        return np.zeros(1)
+
+    monkeypatch.setattr(target=imageio, name="imread", value=mock_imread)
+
+    def mock_rglob(*_, **__):
+        files = [
             pathlib.Path("file___1.jpeg"),
             pathlib.Path("file___2.jpeg"),
         ]
-    )
+        return files
 
-    mocked_imread = MagicMock(return_value=np.array([0]))
+    monkeypatch.setattr(target=pathlib.Path, name="rglob", value=mock_rglob)
 
-    with patch.object(pathlib.Path, "rglob", mocked_rglob):
-        with patch("imageio.imread", mocked_imread):
-            result = file_handling.load_frames(frame_dir=frame_dir, df_frames=df_frames)
+    result = file_handling.load_frames(frame_dir=frame_dir, df_frames=df_frames)
 
     assert len(result) == len(expected)
     for frame_ind, result_frame_dict in result.items():
@@ -50,26 +46,24 @@ def test_load_frames(frame_dir, df_frames, expected):
         assert result_frame_dict["target"] == expected[frame_ind]["target"]
 
 
-@pytest.mark.parametrize(
-    argnames=["video_name", "dst_root_dir", "frames_dict"],
-    argvalues=[
-        [
-            "A",
-            pathlib.Path("dst_dir"),
-            {1: {"image": np.array([0]), "target": "class"}, 2: {"image": np.array([0]), "target": "background"}},
-        ]
-    ],
-)
-def test_save_frames(video_name, dst_root_dir, frames_dict):
+def test_save_frames(monkeypatch):
     """Test case for save_frames function.
 
     Args:
-        video_name: Name of video
-        dst_root_dir: Path to destination root directory
-        frames_dict: Frames dictionary to save
+        monkeypatch: Pytest monkeypatch fixture
     """
-    with patch.object(pathlib.Path, "mkdir") as mkdir_mock:
-        with patch("imageio.imwrite") as imwrite_mock:
-            file_handling.save_frames(video_name=video_name, dst_root_dir=dst_root_dir, frames_dict=frames_dict)
-            assert imwrite_mock.call_count == len(frames_dict)
-            assert mkdir_mock.call_count == 1
+    video_name = "A"
+    dst_root_dir = pathlib.Path("dst_dir")
+    frames_dict = {1: {"image": np.array([0]), "target": "class"}, 2: {"image": np.array([0]), "target": "background"}}
+
+    def mock_mkdir(*_, **__):
+        pass
+
+    monkeypatch.setattr(target=pathlib.Path, name="mkdir", value=mock_mkdir)
+
+    def mock_imwrite(*_, **__):
+        pass
+
+    monkeypatch.setattr(target=imageio, name="imwrite", value=mock_imwrite)
+
+    file_handling.save_frames(video_name=video_name, dst_root_dir=dst_root_dir, frames_dict=frames_dict)
