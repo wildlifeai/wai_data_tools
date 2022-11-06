fa6a2f6033c33b22e4cfba8131ea3c6296fb78f5
davidaderup <david.aderup@gmail.com>
2022-11-06 15:08:52 +0100

    commented ai8x while debugging

--- maxim_datasets/wildlifeai.py
+++ maxim_datasets/wildlifeai.py
@@ -1,60 +1,71 @@
+"""Wildlife.ai dataloader functions."""
 import pathlib
-import pandas as pd
-
 from typing import Callable
-from torchvision import transforms
+
+import pandas as pd
 from PIL import Image
 from torch.utils.data import Dataset
-import ai8x
+from torchvision import transforms
+
+# import ai8x
 
 
 class WildlifeDataset(Dataset):
-    def __init__(self, dataset_dir: pathlib.Path, train: bool,  transforms: Callable) -> None:
-        """Initialise object. """
+    """Dataset object for wildlife.ai images."""
+
+    def __init__(self, dataset_dir: pathlib.Path, train: bool, transform: Callable) -> None:
+        """Initialise object."""
         # load csv with labels
         dataframe = pd.read_csv(dataset_dir)
         split_key = "Train" if train else "Test"
         self.dataframe = dataframe[dataframe["split"] == split_key]
         self.dataset_dir = dataset_dir
-        self.transforms = transforms
+        self.transform = transform
 
     def __getitem__(self, item):
+        """Get item."""
         image_path = self.dataset_dir / self.dataframe.loc[item, "filename"]
-        with Image.open(image_path) as im:
-            input_image = im.load()
-            input_image = self.transforms(input_image)
+        with Image.open(image_path) as image:
+            input_image = image.load()
+            input_image = self.transform(input_image)
         label = self.dataframe.loc[item, "label"]
         return input_image, label
 
     def __len__(self):
+        """Get size of dataset."""
         return len(self.dataframe)
 
 
 def get_wildlifeai_dataset(data, load_train=True, load_test=True):
+    """Get dataset."""
     (data_dir, args) = data
 
     if load_train:
-        train_transform = transforms.Compose([
-            transforms.RandomCrop(48, padding=4),
-            transforms.RandomAffine(degrees=20, translate=(0.1, 0.1), shear=5),
-            transforms.ToTensor(),
-            ai8x.normalize(args=args)
-        ])
+        train_transform = transforms.Compose(
+            [
+                transforms.RandomCrop(48, padding=4),
+                transforms.RandomAffine(degrees=20, translate=(0.1, 0.1), shear=5),
+                transforms.ToTensor(),
+                # ai8x.normalize(args=args)
+            ]
+        )
 
-        train_dataset = WildlifeDataset(dataset_dir=data_dir, train=True, transforms=train_transform)
+        train_dataset = WildlifeDataset(dataset_dir=data_dir, train=True, transform=train_transform)
     else:
         train_dataset = None
 
     if load_test:
-        test_transform = transforms.Compose([
-            transforms.ToTensor(),
-            ai8x.normalize(args=args)
-        ])
+        test_transform = transforms.Compose(
+            [
+                transforms.ToTensor(),
+                # ai8x.normalize(args=args)
+            ]
+        )
 
-        test_dataset = WildlifeDataset(dataset_dir=data_dir, train=False, transforms=test_transform)
+        test_dataset = WildlifeDataset(dataset_dir=data_dir, train=False, transform=test_transform)
 
         if args.truncate_testset:
-            test_dataset.data = test_dataset.data[:1]
+            test_dataset.data = test_dataset.data[:1]  # pylint: disable=attribute-defined-outside-init
     else:
         test_dataset = None
 
