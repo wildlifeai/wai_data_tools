99fde64d97ccbed8e114d33fa29e9b6903327829
davidaderup <46894756+davidaderup@users.noreply.github.com>
2022-11-12 15:52:35 +0100

    Maxim dataset (#13)
    * added first version of maxim dataset code
    
    * commented ai8x while debugging
    
    * modified changes to dataloader for wildlife images

---
+++ maxim_datasets/wildlifeai.py
@@ -0,0 +1,72 @@
+"""Wildlife.ai dataloader functions."""
+import pathlib
+from typing import Callable
+
+import pandas as pd
+from PIL import Image
+from torch.utils.data import Dataset
+from torchvision import transforms
+
+# import ai8x
+
+
+class WildlifeDataset(Dataset):
+    """Dataset object for wildlife.ai images."""
+
+    def __init__(self, dataset_dir: pathlib.Path, train: bool, transform: Callable) -> None:
+        """Initialise object."""
+        # load csv with labels
+        dataframe = pd.read_csv(dataset_dir / "frame_information.csv")
+        split_key = "Train" if train else "Test"
+        self.dataframe = dataframe[dataframe["split"] == split_key].reset_index()
+        self.dataset_dir = dataset_dir
+        self.transform = transform
+
+    def __getitem__(self, item):
+        """Get item."""
+        image_path = (
+            self.dataset_dir
+            / "dataset"
+            / self.dataframe.loc[item, "video_name"]
+            / self.dataframe.loc[item, "file_name"]
+        )
+        with Image.open(image_path) as image:
+            input_image = self.transform(image)
+        label = self.dataframe.loc[item, "label"]
+        return input_image, label
+
+    def __len__(self):
+        """Get size of dataset."""
+        return len(self.dataframe)
+
+
+def get_wildlifeai_dataset(data, load_train=True, load_test=True):
+    """Get dataset."""
+    (data_dir, args) = data
+
+    if load_train:
+        train_transform = transforms.Compose(
+            [
+                transforms.RandomAffine(degrees=20, translate=(0.1, 0.1), shear=5),
+                transforms.ToTensor(),
+                ai8x.normalize(args=args),  # pylint: disable=undefined-variable
+            ]
+        )
+
+        train_dataset = WildlifeDataset(dataset_dir=data_dir, train=True, transform=train_transform)
+    else:
+        train_dataset = None
+
+    if load_test:
+        test_transform = transforms.Compose(
+            [transforms.ToTensor(), ai8x.normalize(args=args)]  # pylint: disable=undefined-variable
+        )
+
+        test_dataset = WildlifeDataset(dataset_dir=data_dir, train=False, transform=test_transform)
+
+        if args.truncate_testset:
+            test_dataset.data = test_dataset.data[:1]  # pylint: disable=attribute-defined-outside-init
+    else:
+        test_dataset = None
+
+    return train_dataset, test_dataset
